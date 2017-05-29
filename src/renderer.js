367a6271a3f617e3077ab249663d8996e7e4425c
Kim Burgess <kim@acaprojects.com>
2017-05-29 13:51:00 +1000

    Output events to console

--- src/renderer.js
+++ src/renderer.js
@@ -1,6 +1,31 @@
-const skype = require('skype-native');
+const { skype } = require('skype-native');
 
 window.skype = skype;
 
-console.log('Attempting outgoing call');
-skype.call('test@example.com');
+console.log(skype);
+
+skype.on('incoming', (inviter, accept, reject) => {
+    console.log(`Incoming call from ${inviter}`);
+    window.accept = accept;
+    window.reject = reject;
+});
+
+skype.on('connected', participants => {
+    console.log(`Connected to ${participants}`);
+});
+
+skype.on('disconnected', () => {
+    console.log('Disconnected');
+});
+
+skype.on('mute', state => {
+    console.log(`Privacy mute changed to ${state}`);
+});
+
+skype.on('muted', () => {
+    console.log(`Privacy mute on`);
+});
+
+skype.on('unmuted', () => {
+    console.log(`Privacy mute off`);
+});
